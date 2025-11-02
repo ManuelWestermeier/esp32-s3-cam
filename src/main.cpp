80616ea2a8d758171cf8da6b13109754b6002549
manuelwestermeier <westermeier111@gmail.com>
2025-11-02 21:23:58 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,23 @@ const char *ssid = "io";
 const char *password = "hhhhhh90";
 
 // PIN CONFIG for Goouuu ESP32-S3-CAM
-#define PWDN_GPIO_NUM -1
+// camera_pins.h (example AI_THINKER)
+#define PWDN_GPIO_NUM 32
 #define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 40
-#define SIOD_GPIO_NUM 17
-#define SIOC_GPIO_NUM 18
-
-#define Y9_GPIO_NUM 39
-#define Y8_GPIO_NUM 41
-#define Y7_GPIO_NUM 42
-#define Y6_GPIO_NUM 12
-#define Y5_GPIO_NUM 3
-#define Y4_GPIO_NUM 14
-#define Y3_GPIO_NUM 47
-#define Y2_GPIO_NUM 13
-#define VSYNC_GPIO_NUM 38
-#define HREF_GPIO_NUM 48
-#define PCLK_GPIO_NUM 11
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
 WebServer server(80);
 
