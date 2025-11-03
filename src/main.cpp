c83439d4082e5acb92d2f7b213807a19a0879fe0
manuelwestermeier <westermeier111@gmail.com>
2025-11-03 23:12:09 +0100

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,21 @@ const char *password = "hhhhhh90";
 // Prüfe deine Platine, ggf. Pins anpassen
 #define PWDN_GPIO_NUM -1
 #define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 40
+#define XCLK_GPIO_NUM 15
 #define SIOD_GPIO_NUM 4
-#define SIOC_GPIO_NUM 18
+#define SIOC_GPIO_NUM 5
 
-#define Y9_GPIO_NUM 39
-#define Y8_GPIO_NUM 41
-#define Y7_GPIO_NUM 42
+#define Y9_GPIO_NUM 16
+#define Y8_GPIO_NUM 17
+#define Y7_GPIO_NUM 18
 #define Y6_GPIO_NUM 12
-#define Y5_GPIO_NUM 3
-#define Y4_GPIO_NUM 14
-#define Y3_GPIO_NUM 47
-#define Y2_GPIO_NUM 13
-#define VSYNC_GPIO_NUM 38
-#define HREF_GPIO_NUM 48
-#define PCLK_GPIO_NUM 11
+#define Y5_GPIO_NUM 10
+#define Y4_GPIO_NUM 8
+#define Y3_GPIO_NUM 9
+#define Y2_GPIO_NUM 11
+#define VSYNC_GPIO_NUM 6
+#define HREF_GPIO_NUM 7
+#define PCLK_GPIO_NUM 13
 
 WebServer server(80);
 
